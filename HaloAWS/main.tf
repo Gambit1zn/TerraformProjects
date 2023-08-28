85bfa3975df57b5abf6e6388bfa4d8d97af27698
Ethan Ankadu <ethanankadu1zn@gmail.com>
2023-08-28 22:39:09 +0200

    tested main vpc creation with subnets and gateway. Need to add associations and routes for public subnet and private subnets

--- HaloAWS/main.tf
+++ HaloAWS/main.tf
@@ -14,28 +14,84 @@ provider "aws" {
 
 resource "aws_vpc" "halo_terraform_vpc" {
   cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "halo-terraform"
+  }
 }
 
 resource "aws_subnet" "halo_terraform_public_subnet_1" {
   vpc_id = aws_vpc.halo_terraform_vpc.id
   cidr_block = "10.0.0.0/28"
   availability_zone = "ca-central-1a"
+
+  depends_on = [ aws_vpc.halo_terraform_vpc ]
+
+  tags = {
+    Name = "halo-terraform-subnet-public1-ca-central-1a"
+  }
 }
 
 resource "aws_subnet" "halo_terraform_public_subnet_2" {
   vpc_id = aws_vpc.halo_terraform_vpc.id
   cidr_block = "10.0.0.16/28"
   availability_zone = "ca-central-1b"
+
+  depends_on = [ aws_vpc.halo_terraform_vpc ]
+
+    tags = {
+    Name = "halo-terraform-subnet-public2-ca-central-1b"
+  }
 }
 
 resource "aws_subnet" "halo_terraform_private_subnet_1" {
   vpc_id = aws_vpc.halo_terraform_vpc.id
   cidr_block = "10.0.0.32/27"
   availability_zone = "ca-central-1a"
+
+  depends_on = [ aws_vpc.halo_terraform_vpc ]
+
+    tags = {
+    Name = "halo-terraform-subnet-private1-ca-central-1a"
+  }
 }
 
 resource "aws_subnet" "halo_terraform_private_subnet_2" {
   vpc_id = aws_vpc.halo_terraform_vpc.id
   cidr_block = "10.0.0.64/27"
   availability_zone = "ca-central-1b"
+
+  depends_on = [ aws_vpc.halo_terraform_vpc ]
+
+    tags = {
+    Name = "halo-terraform-subnet-private1-ca-central-1b"
+  }
+}
+
+resource "aws_internet_gateway" "halo-terraform-igw" {
+  vpc_id = aws_vpc.halo_terraform_vpc.id
+
+  tags = {
+    Name = "halo-terraform-igw"
+  }
+
+  depends_on = [ aws_vpc.halo_terraform_vpc ]
+}
+
+# resource "aws_internet_gateway_attachment" "halo-terraform-igw-attachment" {
+#   internet_gateway_id = aws_internet_gateway.halo-terraform-igw.id
+#   vpc_id = aws_vpc.halo_terraform_vpc.id
+
+#   depends_on = [ aws_internet_gateway.halo-terraform-igw ]
+# }
+
+resource "aws_route_table" "PublicRouteTable" {
+  vpc_id = aws_vpc.halo_terraform_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.halo-terraform-igw.id
+  }
+  tags = {
+    Name = "PublicRouteTable"
+  }
 }
